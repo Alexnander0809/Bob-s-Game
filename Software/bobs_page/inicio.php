aab204fbd572d13ec138bb2105238e782b7c6a93
Alexander0809 <113730683+Alexnander0809@users.noreply.github.com>
2022-09-22 09:21:45 -0500

    Add files via upload

---
+++ Software/bobs_page/inicio.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+	<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+		<script src="js/main.js"></script>
+		<title>Principal - Bob's Game</title>
+	</head>
+	<body>
+		<div class="container">
+			<nav>
+				<div class="logo">
+				<a href="../bobs_game/game.php"><i class="fab fa-BoBs_games"><img src="img/logo.png" width="40" height="40"></i></a>
+				</div>
+			</nav>
+			<div class="right-main">
+				<div class="main-top">
+					<div class="search">
+						<i class="fas fa-search"></i>
+						<input type="text" placeholder="Buscar"/>
+					</div>
+					<div class="top">
+						<a href="../bobs_login"><i class="fas fa-user-circle"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+										<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+										<path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+								</svg>
+							</i>
+						</a>
+					</div>
+				</div>
+				<div class="main-flex">
+					<main>
+						<header>
+							<div class="tag">Nuevo</div>
+							<h1>Marvel's Spider-Man: Myles Morals</h1>
+							<div class="purchase">
+								<h2>$200.000</h2>
+								<a href="../bobs_sec/detalles pago/detalles.php"><button>Comprar</button></a>
+							</div>
+							<div class="spiderman-img">
+								<img src="img/spiderman.png" alt="spiderman" draggable="false" />
+							</div>
+						</header>
+						<div class="most-played">
+							<h3>Juegos populares</h3>
+							<div class="cards">
+								<div class="games-card">
+									<div class="game-image-container">
+										<img src="img/Assassins-Creed-Valhalla.jpg" alt="ac-valhalla" />
+									</div>
+
+									<h4>Assassins Creed Valhalla</h4>
+									<p>$534.000</p>
+								</div>
+								<div class="games-card">
+									<div class="game-image-container">
+										<img src="img/Cyberpunk-2077.jpg" alt="cyberpunk" />
+									</div>
+									<h4>Cyberpunk 2077</h4>
+									<p>$667.000</p>
+								</div>
+								<div class="games-card">
+									<div class="game-image-container">
+										<img src="img/far-cry-5-artwork-joseph-seed-uhd-8k-wallpaper.jpg" alt="Farcry5" />
+									</div>
+									<h4>Far Cry 5</h4>
+									<p>$445.000</p>
+								</div>
+							</div>
+						</div>
+					</main>
+					<aside>
+						<div class="library">
+							<h3>Juegos</h3>
+							<div class="library-card">
+								<div class="game-logo">
+									<img src="img/RDR.png" alt="" />
+								</div>
+								<div class="game-details">
+									<h4>Red Dead Redemption</h4>
+									<p>PS4</p>
+								</div>
+								<i class="fas fa-download"></i>
+							</div>
+							<div class="library-card">
+								<div class="game-logo">
+									<img src="img/GTA-V-Premium-Edition-Free-Giveaway-Epic-Games-Store.jpg" alt="" />
+								</div>
+								<div class="game-details">
+									<h4>Gran Theft Auto V</h4>
+									<p>PS4 & PS5</p>
+								</div>
+								<i class="far fa-check-circle"></i>
+							</div>
+							<div class="library-card">
+								<div class="game-logo">
+									<img src="img/Fifa.png" alt="" />
+								</div>
+								<div class="game-details">
+									<h4>Fifa 21</h4>
+									<p>PS4</p>
+								</div>
+								<i class="fas fa-download"></i>
+							</div>
+							<hr />
+							<h3>Amigos</h3>
+							<div class="library-card">
+								<div class="game-logo">
+									<img src="img/avatar.png" alt="" />
+								</div>
+								<div class="game-details">
+									<h4>D4V150N</h4>
+									<p>Jugando Resident Evil Village</p>
+								</div>
+								<div class="online"></div>
+							</div>
+							<div class="library-card">
+								<div class="game-logo">
+									<img src="img/3219841.png" alt="" />
+								</div>
+								<div class="game-details">
+									<h4>Μαργαρίτα</h4>
+									<p>Offline desde hace 11 meses</p>
+								</div>
+								<div class="offline"></div>
+							</div>
+							<div class="library-card">
+								<div class="game-logo">
+									<img src="img/avatar-373-456325.png" alt="" />
+								</div>
+								<div class="game-details">
+									<h4>Alex</h4>
+									<p>Jugando The Witcher 3: Wild Hunt</p>
+								</div>
+								<div class="online"></div>
+							</div>
+						</div>
+					</aside>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
