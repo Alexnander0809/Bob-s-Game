6175a20e66884e37d66ea6ef0e4228fc8527d4f7
Alexander_08 <alexvalderramaguisao@gmail.com>
2024-07-08 17:11:56 -0500

    Ya corre la page

---
+++ software/client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/" Component={Home} />
+        <Route exact path="/" Component={Home} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
--- software/client/src/components/slider.jsx
+++ software/client/src/components/slider.jsx
@@ -1,151 +1,67 @@
-import React, { Component } from 'react';
-import { TweenMax, TimelineMax, Expo, Power4, Linear } from 'gsap';
-import * as THREE from 'three';
+// Slider.jsx
 
-class Slider extends Component {
-  constructor(props) {
-    super(props);
+import React, { useRef } from 'react';
+import { TweenMax, Power3 } from 'gsap';
 
-    this.bindAll();
+const Slider = () => {
+    const sliderRef = useRef(null);
 
-    this.vert = `
-      varying vec2 vUv;
-      void main() {
-        vUv = uv;
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-      }
-    `;
-
-    this.frag = `
-      varying vec2 vUv;
-
-      uniform sampler2D texture1;
-      uniform sampler2D texture2;
-      uniform sampler2D disp;
-
-      uniform float dispPower;
-      uniform float intensity;
-
-      uniform vec2 size;
-      uniform vec2 res;
-
-      vec2 backgroundCoverUv( vec2 screenSize, vec2 imageSize, vec2 uv ) {
-        float screenRatio = screenSize.x / screenSize.y;
-        float imageRatio = imageSize.x / imageSize.y;
-        vec2 newSize = screenRatio < imageRatio 
-          ? vec2(imageSize.x * (screenSize.y / imageSize.y), screenSize.y)
-          : vec2(screenSize.x, imageSize.y * (screenSize.x / imageSize.x));
-        vec2 newOffset = (screenRatio < imageRatio 
-          ? vec2((newSize.x - screenSize.x) / 2.0, 0.0) 
-          : vec2(0.0, (newSize.y - screenSize.y) / 2.0)) / newSize;
-        return uv * screenSize / newSize + newOffset;
-      }
-
-      void main() {
-        vec2 uv = vUv;
-        
-        vec4 disp = texture2D(disp, uv);
-        vec2 dispVec = vec2(disp.x, disp.y);
-        
-        vec2 distPos1 = uv + (dispVec * intensity * dispPower);
-        vec2 distPos2 = uv + (dispVec * -(intensity * (1.0 - dispPower)));
-        
-        vec4 _texture1 = texture2D(texture1, distPos1);
-        vec4 _texture2 = texture2D(texture2, distPos2);
-        
-        gl_FragColor = mix(_texture1, _texture2, dispPower);
-      }
-    `;
-
-    this.state = {
-      animating: false,
-      initial: true
+    const scrollToSlide = (index) => {
+        const offset = -index * sliderRef.current.clientHeight;
+        TweenMax.to(sliderRef.current, 0.5, {
+            y: offset,
+            ease: Power3.easeInOut,
+        });
     };
 
-    this.images = [
-      'backgroundimgshere',
-      'backgroundimgshere',
-      'backgroundimgshere'
-    ];
-
-    this.textures = [];
-    this.renderer = null;
-    this.scene = null;
-    this.clock = null;
-    this.camera = null;
-    this.mat = null;
-    this.disp = null;
-
-    this.slides = [];
-    this.bullets = [];
-
-    this.data = {
-      current: 0,
-      next: 1,
-      total: this.images.length - 1,
-      delta: 0
-    };
-  }
-
-  componentDidMount() {
-    this.init();
-  }
-
-  componentWillUnmount() {
-    // Limpiar recursos si es necesario
-  }
-
-  bindAll() {
-    ['render', 'nextSlide'].forEach(fn => this[fn] = this[fn].bind(this));
-  }
-
-  setStyles() {
-    // Implementación de setStyles
-  }
-
-  cameraSetup() {
-    // Implementación de cameraSetup
-  }
-
-  setup() {
-    // Implementación de setup
-  }
-
-  loadTextures() {
-    // Implementación de loadTextures
-  }
-
-  createMesh() {
-    // Implementación de createMesh
-  }
-
-  transitionNext() {
-    // Implementación de transitionNext
-  }
-
-  prevSlide() {
-    // Implementación de prevSlide
-  }
-
-  nextSlide() {
-    // Implementación de nextSlide
-  }
-
-  changeTexture() {
-    // Implementación de changeTexture
-  }
-
-  listeners() {
-    // Implementación de listeners
-  }
-
-  render() {
     return (
-      <div className="js-slider">
-        {/* HTML necesario para el slider */}
-      </div>
+        <div ref={sliderRef} className="slider__inner">
+            <div className="slide" onClick={() => scrollToSlide(0)}>
+                <div className="slide__content">
+                    <figure className="slide__img js-slide__img">
+                        <img src="/image1.jpg" alt="Slide 1" />
+                    </figure>
+                    <figure className="slide__img js-slide__img">
+                        <img src="/image2.jpg" alt="Slide 1" />
+                    </figure>
+                </div>
+                <div className="slider__text js-slider__text">
+                    <div className="slider__text-line js-slider__text-line">
+                        <div>Slide 1 Text</div>
+                    </div>
+                    <div className="slider__text-line js-slider__text-line">
+                        <div>Slide 1 Text</div>
+                    </div>
+                    <div className="slider__text-line js-slider__text-line">
+                        <div>Slide 1 Text</div>
+                    </div>
+                    <div className="slider__text-line js-slider__text-line">
+                        <div>Slide 1 Text</div>
+                    </div>
+                </div>
+            </div>
+            <div className="slide" onClick={() => scrollToSlide(1)}>
+                <div className="slide__content">
+                    <figure className="slide__img js-slide__img">
+                        <img src="/image3.jpg" alt="Slide 2" />
+                    </figure>
+                    <figure className="slide__img js-slide__img">
+                        <img src="/image4.jpg" alt="Slide 2" />
+                    </figure>
+                </div>
+            </div>
+            <div className="slide" onClick={() => scrollToSlide(2)}>
+                <div className="slide__content">
+                    <figure className="slide__img js-slide__img">
+                        <img src="/image5.jpg" alt="Slide 3" />
+                    </figure>
+                    <figure className="slide__img js-slide__img">
+                        <img src="/image6.jpg" alt="Slide 3" />
+                    </figure>
+                </div>
+            </div>
+        </div>
     );
-  }
-}
+};
 
 export default Slider;
